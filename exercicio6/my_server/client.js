b0b8f3d03103d39b61fc1287c7f38a2d1898b796
Gabriel Maciel <gmc2@cin.ufpe.br>
2023-03-16 11:25:21 -0300

    Add files via upload
    Correção de algumas coisas e renomear alguns arquivos

--- exercicio6/my_server/client.js
+++ exercicio6/my_server/client.js
@@ -1,6 +1,6 @@
 const socket = new WebSocket('ws://localhost:8080');
 
-const output = document.getElementById('output');
+const feed = document.getElementById('feed');
 const nicknameInput = document.getElementById('nickname');
 const messageInput = document.getElementById('message');
 const sendButton = document.getElementById('send');
@@ -12,4 +12,15 @@ sendButton.addEventListener('click', () => {
 
     socket.send(JSON.stringify(data));
     messageInput.value = '';
+});
+
+socket.addEventListener('message', async (event) => {
+    console.log('recebeu de outros clientes')
+    const text = await new Response(event.data).text();
+
+    let div = document.getElementById("feed");
+    div.append(text);
+
+    var linebreak = document.createElement('br');
+    div.appendChild(linebreak);
 });
